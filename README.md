e07fefe505930b3162e5a1e8e26574fe774dd848
awsk1994 <awsk1994@gmail.com>
2015-11-29 19:06:33 -0500

    milestone udpate
     - added socket io
     - layer problem when receive new json message
     - changed data.json from client to server side

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ Leaflet: http://leafletjs.com/
 Leaflet plugin (indoor-view, with floor changer): https://github.com/cbaines/leaflet-indoor
 
 ---
+
+Milestone4(a): 
+ -> right now, the layers (of tables with different colors) overlap everytime there is a new json data sent via socket io. Need to fix it so it replaces or clears before adding more layers each time a new json file comes in.
+ -> you can use localhost:3000/test1.html to intercept messages via socket io between host and client, or simply type table id to change the table color.
--- milestone4(a)/signup.js
+++ milestone4(a)/signup.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var sql = require ('sql.js');
 var fs = require('fs');
+var http = require('http').Server(app); // start an http server
+var io = require('socket.io')(http);
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -13,6 +15,7 @@ app.get('/', function (req, res)
 	res.sendFile(__dirname + '/static_files/map_test.html');
 
 });
+
 app.use(express.static('static_files'));
 
 var sqliteExists = 1;
@@ -38,9 +41,76 @@ else
 	var db = new sql.Database(filebuffer);
 }
 
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync(__dirname + '/data.json'));
 //Default user - so there is something in the data.
 //var data = [
 //{name: 'Jin', password: 'Ann'}];
+
+
+
+
+
+io.on('connection', function(socket)
+{
+  // when *any* user sends a 'chat message' to the server, run the inner
+  // function below ...
+  // (note that this inner function runs only when someone sends a message to
+  // the server. if nobody is sending messages, this function never runs)
+  socket.on('chat message', function(msg)
+  {
+    // emit (send) the message msg verbatim to ALL BROWSERS
+    // that are currently connected to the server at this moment
+    console.log("receive message message: " + msg)
+
+	var tableToLookup = msg;
+	console.log("input: " + msg);
+	if(tableToLookup == 0)
+	{
+		io.emit('chat message', JSON.stringify(obj));
+		//return;
+	}
+	else
+	{
+		for( var i = 0; i < obj.features.length; i++) 
+		{
+			var e = obj.features[i];
+			if (e.id.toString() == tableToLookup) 
+			{
+				console.log("table to change: " + tableToLookup + ": Stat: " + e.occupied);
+
+				if (e.occupied == 1) {
+					// e.occupied = 0;
+					obj.features[i].occupied = 0;
+				} 
+				else 
+				{
+					// e.occupied = 1;
+					obj.features[i].occupied = 1;
+				}
+
+				console.log("table changed: " + e.id.toString() + ": Stat: " + e.occupied);
+				//console.log("changed: " + obj.features[i].geometry.coordinates + ": " + obj.features[i].occupied);
+				fs.writeFileSync(__dirname + '/data.json', JSON.stringify(obj));
+				//res.send(obj);
+			    io.emit('chat message', JSON.stringify(obj));
+
+				//return;
+			}
+			// res.send();
+			// return;
+		}
+	}
+	// res.send(JSON.stringify(obj.features.length));
+	//res.send('ERROR');
+	//o.emit('chat message', "ERROR");
+
+  });
+});
+
+
+
+
 
 
 
@@ -110,6 +180,22 @@ app.get('/users/*', function(req, res)
 	return;
 });
 
+//READ ALL TABLES DATA
+// app.get('/table', function(req, res)
+// {
+// 	var allTables = [];
+// 	console.log("1");
+	// var stmt = 
+// });
+
+//REAd ONE TALBLE
+app.get('/table', function(req, res)
+{
+	res.send(obj);
+	//res.send(JSON.stringify(obj));
+	return;
+});
+
 //UPDATE USER PROFILE
 app.put('/users/*', function (req, res)
 {
@@ -128,6 +214,34 @@ app.put('/users/*', function (req, res)
 	return;
 });
 
+app.put('/table/*', function (req, res) 
+{
+	var tableToLookup = req.params[0];
+	console.log("input: " + req.params[0]);
+	for( var i = 0; i < obj.features.length; i++) {
+	// for (var i in obj.features ) {
+		var e = obj.features[i];
+		if (e.id.toString() == tableToLookup) {
+			if (e.occupied == 1) {
+				// e.occupied = 0;
+				obj.features[i].occupied = 0;
+			} else {
+				// e.occupied = 1;
+				obj.features[i].occupied = 1;
+			}
+			//console.log("changed: " + obj.features[i].geometry.coordinates + ": " + obj.features[i].occupied);
+			fs.writeFileSync(__dirname + '/data.json', JSON.stringify(obj));
+			res.send(obj);
+			return;
+		}
+		// res.send();
+		// return;
+	}
+	// res.send(JSON.stringify(obj.features.length));
+	res.send('ERROR');
+
+});
+
 //DELETE USER PROFILE
 app.delete('/users/*', function (req, res)
 {
@@ -140,9 +254,8 @@ app.delete('/users/*', function (req, res)
 	res.send('OK');
 });
 
-var server = app.listen(3000, function()
+var server = http.listen(3000, function()
 {	
 	var port = server.address().port;
 	console.log('Server started at http://localhost:%s/', port);
 });
-
