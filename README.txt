a45bc5be50f3799ab5943ef1140e90a20393a525
Alex Wong <awsk1994@gmail.com>
2016-03-05 16:12:20 -0500

    minor changes and comment changes

--- README.txt
+++ README.txt
@@ -5,21 +5,14 @@ This application contains a server, and several html files that acts as the clie
 
 The client-side will login by checking with the server’s database regarding to user information. After you log in, you can access a map interface which will retrieve and allow users to change table availability as well as printer queue. These information are stored on the server-side, and the client-side will have to request the information and update the view on the browser accordingly. 
 
-Future implementations of this applications can expand to: 
-(a) other libraries
-(b) usage with a system to control printer queue (e.g: when a student prints something, it increments the printer queue, and decrements after student is completed)
-(c) include user information when updating a table
-(d) enable tables to move around, which will be controlled by an administrator (such as a librarian)
-(e) queue-related events (such as the line in a cafeteria, or how many people have entered a buffet).
-
-
 TEST-RUN:
-1. In terminal, navigate to folder and enter: node server.js
+1. In terminal, navigate to folder and enter: 
+		node server.js
 2 (optional). In terminal, navigate to same folder and enter: ./ngrok http 3000 
-{
-This emits the local server onto the web via a temporary URL.
-After you do this step, you can use the temporary URL shown on the terminal to go on the page. At the same time, other users can use this URL to log in and will be sharing the same database. If you do not do this step, then you will have to implement some sort of web-hosting application, or else, the server will only run on localhost:3000.
-}
+	{
+	This emits the local server onto the web via a temporary URL.
+	After you do this step, you can use the temporary URL shown on the terminal to go on the page. At the same time, other users can use this URL to log in and will be sharing the same database. If you do not do this step, then you will have to implement some sort of web-hosting application, or else, the server will only run on localhost:3000.
+	}
 3. On browser:
 	(a) Go to localhost:3000 (if didn’t do STEP 2)
   	(b) Go to http://<random numbers>.ngrok.io shown on terminal if you run ngrok (if you do STEP 2)
@@ -57,3 +50,11 @@ signup.html: sign up page
 
 editinfo.html: edit information page (needs to sign in first)
 deleted.html: deleted user page (when user clicks delete button after signing in)
+
+FUTURE IMPLEMENTATION of this applications can expand to: 
+(a) other libraries
+(b) usage with a system to control printer queue (e.g: when a student prints something, it increments the printer queue, and decrements after student is completed)
+(c) include user information when updating a table
+(d) enable tables to move around, which will be controlled by an administrator (such as a librarian)
+(e) queue-related events (such as the line in a cafeteria, or how many people have entered a buffet).
+
--- server.js
+++ server.js
@@ -1,23 +1,143 @@
+
+/*
+================================
+	EXPLANATION
+================================
+Upon running server.js, one will start a server on a specific port (default is port 3000). 
+Client side can access server, and will be re-directed to signin.html (which is in the static_files directory).
+Server-client side communication involves a sqlite database for account info, and 2 json files, each containing printer and table information.
+
+PART A: CRUD (Create, Read, Update, Delete) -- Account Info.
+Client side (html) will communicate with the server's CRUD to access/edit/delete account info. 
+
+PART B: Socket Communication with Client -- Table and Printer Info.
+When accessing map.html (the map interface of the table availability in the library), client and server communicate via the socket.
+
+The client will send a message to the server in two situations:
+	 - client changes a data in table and/or printer database
+	 - client just connected (eg: a new client logs into map.html): 
+	 		 * Since client will not have info of the updated database at this point, it will send a message containing '0', which basically informs the server new client connected. 
+	 		 * Server will then emit a message to all client(s) of the most updated database. 
+
+When a client detects a change in the table and/or printer database, it will send a message in the socket.
+The server will receive the message from the client(s), and update two sources:
+	 - local (table and/or printer) json file; done by overwriting the json file 
+	 - other clients; done by sending a message to ALL clients of the new table/printer database.
+
+Programmed by Alex Wong and Jin Chul Ann
+*/
+
+// ====== IMPORT node_modules ======
 var express = require('express');
 var app = express();
 var sql = require ('sql.js');
 var fs = require('fs');
 var http = require('http').Server(app); // start an http server
 var io = require('socket.io')(http);
-var jsonfile = require('jsonfile');
+var jsonfile = require('jsonfile');		//to read json file
 
+
+// ====== SETUP ======
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Load files in directory: static files
+app.use(express.static('static_files'));
+
+//Automatically redirect webpage to signin.html.
 app.get('/', function (req, res)
 {
 	res.sendFile(__dirname + '/static_files/signin.html');
 });
 
-app.use(express.static('static_files'));
 
-var sqliteExists = 1;
+// ====== PART A: CRUD (Create, Read, Update, Delete) -- Account Info ======
+
+// CREATE USER
+app.post('/users', function(req, res)
+{
+	var postBody = req.body;
+	var myName = postBody.name;
+
+	if(!myName)
+	{
+		res.send('ERROR');
+		return;
+	}
+
+	var stmt = db.prepare("SELECT * FROM data WHERE name=:uname", {':uname':myName});
+	if(!stmt.step())
+	{
+		//Insert data into the sqlite database (containing account info)
+		db.run("INSERT INTO data VALUES (:name, :password, :bday, :job, :email)", 
+		{':name':postBody.name, ':password':postBody.password, ':bday':postBody.bday,
+			':job':postBody.job, ':email':postBody.email});
+
+		//Export new data
+		var data = db.export();
+		var buffer = new Buffer(data);
+		fs.writeFileSync("data.sqlite", buffer);
+		res.send('OK');
+	}
+	else
+	{
+		res.send('ERROR_Username');
+		return;
+	}
+});
+
+// READ LIST OF USER
+app.get('/users', function(req, res)
+{
+	var allUsers = [];
+	var stmt = db.prepare("SELECT * FROM data");
+	while (stmt.step())
+		allUsers.push(stmt.getAsObject().name);
+	res.send(allUsers);
+});
+
+// READ SPECIFIC USER
+app.get('/users/*', function(req, res)
+{
+	var stmt = db.prepare("SELECT * FROM data WHERE name=:user");
+	var result = stmt.getAsObject({':user':req.params[0]});
+	res.send(result);
+	return;
+});
+
+//UPDATE USER PROFILE
+app.put('/users/*', function (req, res)
+{
+	var nameToLookup = req.params[0];
+	var postBody = req.body;
+
+	//Update database.
+	for(key in postBody)
+	{
+		db.run("UPDATE data SET '" + key + "'='" + postBody[key] + "' WHERE name='" + nameToLookup + "'");
+	}
+
+	//Export new data
+	var data = db.export();
+	var buffer = new Buffer(data);
+	fs.writeFileSync("data.sqlite", buffer);
+	res.send('OK');
+	return;
+});
+
+// DELETE USER PROFILE
+app.delete('/users/*', function (req, res)
+{
+	var myName = req.params[0];
+	db.run("DELETE from data WHERE name=:uname", {':uname':myName});
+	
+	//Export new data
+	var data = db.export();
+	var buffer = new Buffer(data);
+	fs.writeFileSync("data.sqlite", buffer);
+	res.send('OK');
+});
 
 //Toggle sqliteExists off if there is no sqLite database. But this should be only done by the admin. 
 if(sqliteExists == 0)
@@ -37,41 +157,67 @@ else
 {
 	//Read data
 	var filebuffer = fs.readFileSync('data.sqlite');
+
 	// Load the db
 	var db = new sql.Database(filebuffer);
 }
 
-var fs = require('fs');
-var obj = JSON.parse(fs.readFileSync(__dirname + '/data.json'));
-var printer_data = JSON.parse(fs.readFileSync(__dirname + '/printer.json'));
+// Listen at port 3000.
+var server = http.listen(3000, function()
+{	
+	var port = server.address().port;
+	console.log('Server started at http://localhost:%s/', port);
+});
+
+// ===== PART B: Socket Communication with Client -- Table and Printer Info =====
+
+// If data.sqlite exists, this is toggled on.
+// If toggled off, it will create a sqlite database (data.sqlite)
+// Reference the if-else statement a few lines below.
+var sqliteExists = 1;
+
+//Read json file synchrously 
+var fs = require('fs');		// this is used ot read json file
+var table_data = JSON.parse(fs.readFileSync(__dirname + '/data_table.json'));
+var printer_data = JSON.parse(fs.readFileSync(__dirname + '/data_printer.json'));
 
 //Socket IO connection to client: Printer and Table message.
 io.on('connection', function(socket)
 {
-  // when *any* user sends a 'table' to the server, run the inner
+  // When any user sends a 'table' to the server, run the inner
   // function below ...
   // (note that this inner function runs only when someone sends a message to
   // the server. if nobody is sending messages, this function never runs)
+
+	//Listen in socket for printer messages.
   socket.on('printer', function(msg)
   {
   	console.log("receive printer message: " + msg);
+
+  	//if msg == 0, then this is the initial message when a new client is initialized.
+  	//When this happens, emit a message to all clients to update the data (since the new client will need the update; as it's initial database might be empty). 
   	if(msg == 0)
   	{
-  		console.log("send message out");
-  		io.emit('printer', JSON.stringify(printer_data));
+  		console.log("Send message out");
+  		io.emit('printer', JSON.stringify(printer_data));	//JSON.stringify converts JSON object to a string.
   	}
   	else
   	{
+  		// The format of msg is a '+' or '-', followed by the printer id.
+  		// +<printer_id> indicates an increment in the printer queue; vice versa for -<printer_id>
   		if(msg.charAt(0) == '+' || msg.charAt(0) == '-')
   		{
   			//TODO: Now, printer_num is only limited to one digit.
   			var printer_num = msg.charAt(1);
-  			for( var i = 0; i < printer_data.features.length; i++) 
+
+  			//TOOD: need to loop through all even after found the table_id?
+  			//Loop through printer_data, and find the printer_id. Then change the data (increment/decrement queue).
+  			for( var i = 0; i < printer_data.features.length; i++)
 			{
 				var e = printer_data.features[i];
 				if (e.id.toString() == printer_num) 
 				{
-					console.log("printer to increment: " + printer_num + ": Queue: " + e.queue);
+					console.log("Printer to increment: " + printer_num + ": Queue: " + e.queue);
 
 					if(e.queue == 0 && msg.charAt(0) == '-')
 						console.log("ERROR. Queue already at 0. Cannot decrease");
@@ -84,10 +230,11 @@ io.on('connection', function(socket)
 
 					console.log("Printer Queue changed: " + e.id.toString() + ": Queue: " + e.queue);
 
+					// Send 'printer' message in socket to notify ALL clients.
 				    io.emit('printer', JSON.stringify(printer_data));
 
-				    //Plugin to write file in json format (more neat)
-				    var file = __dirname + "/printer.json";
+				    // Update the file on the server-side; local printer message.
+				    var file = __dirname + "/data_printer.json";
 				    jsonfile.writeFile(file, printer_data, {spaces:2}, function(err)
 			    	{
 			    		console.error(err);
@@ -101,45 +248,47 @@ io.on('connection', function(socket)
   		}
   	}
   });
+
+	// Listen in socket for 'table' messages.
   socket.on('table', function(msg)
   {
-    // emit (send) the message msg verbatim to ALL BROWSERS
-    // that are currently connected to the server at this moment
-    console.log("receive table message: " + msg)
+    console.log("Receive table message: " + msg)
 
 	var tableToLookup = msg;
 	console.log("input: " + msg);
+
+  	//if msg == 0, then this is the initial message when a new client is initialized.
+  	//When this happens, emit a message to all clients to update the data (since the new client will need the update; as it's initial database might be empty). 
 	if(tableToLookup == 0)
 	{
-		io.emit('table', JSON.stringify(obj));
+		io.emit('table', JSON.stringify(table_data));
 	}
 	else
 	{
-		for( var i = 0; i < obj.features.length; i++) 
+		//Loop through JSON to find a match in table_id (in our local json file). Then update client and local file.
+		for( var i = 0; i < table_data.features.length; i++) 
 		{
-			var e = obj.features[i];
+			var e = table_data.features[i];
 			if (e.id.toString() == tableToLookup) 
 			{
 				console.log("table to change: " + tableToLookup + ": Stat: " + e.occupied);
 
 				if (e.occupied == 1) {
-					// e.occupied = 0;
-					obj.features[i].occupied = 0;
+					table_data.features[i].occupied = 0;
 				} 
 				else 
 				{
-					// e.occupied = 1;
-					obj.features[i].occupied = 1;
+					table_data.features[i].occupied = 1;
 				}
 
 				console.log("table changed: " + e.id.toString() + ": Stat: " + e.occupied);
 
-				//res.send(obj);
-			    io.emit('table', JSON.stringify(obj));
+				// Send table socket message to ALL client to update their table database.
+			    io.emit('table', JSON.stringify(table_data));
 
-			    //Plugin to write file in json format (more neat)
-			    var file = __dirname + "/data.json";
-			    jsonfile.writeFile(file, obj, {spaces:2}, function(err)
+			    // Update the local json file.
+			    var file = __dirname + "/data_table.json";
+			    jsonfile.writeFile(file, table_data, {spaces:2}, function(err)
 		    	{
 		    		console.error(err);
 		    	});
@@ -149,92 +298,4 @@ io.on('connection', function(socket)
   });
 });
 
-//CREATE USER
-app.post('/users', function(req, res)
-{
-	var postBody = req.body;
-	var myName = postBody.name;
 
-	if(!myName)
-	{
-		res.send('ERROR');
-		return;
-	}
-
-	var stmt = db.prepare("SELECT * FROM data WHERE name=:uname", {':uname':myName});
-	if(!stmt.step())
-	{
-		db.run("INSERT INTO data VALUES (:name, :password, :bday, :job, :email)", 
-		{':name':postBody.name, ':password':postBody.password, ':bday':postBody.bday,
-			':job':postBody.job, ':email':postBody.email});
-		//export new data
-		var data = db.export();
-		var buffer = new Buffer(data);
-		fs.writeFileSync("data.sqlite", buffer);
-
-		res.send('OK');
-	}
-	else
-	{
-		res.send('ERROR_Username');
-		return;
-	}
-});
-
-//READ LIST OF USER
-app.get('/users', function(req, res)
-{
-	var allUsers = [];
-	console.log("1");
-	var stmt = db.prepare("SELECT * FROM data");
-	console.log("2");
-	while (stmt.step())
-		allUsers.push(stmt.getAsObject().name);
-
-	res.send(allUsers);
-});
-
-//READ SPECIFIC USER
-app.get('/users/*', function(req, res)
-{
-	var stmt = db.prepare("SELECT * FROM data WHERE name=:user");
-	var result = stmt.getAsObject({':user':req.params[0]});
-	res.send(result);
-	return;
-});
-
-//UPDATE USER PROFILE
-app.put('/users/*', function (req, res)
-{
-	var nameToLookup = req.params[0];
-	var postBody = req.body;
-	for(key in postBody)
-		{
-		db.run("UPDATE data SET '" + key + "'='" + postBody[key] + "' WHERE name='"+nameToLookup + "'");
-	}
-	//export new data
-	var data = db.export();
-	var buffer = new Buffer(data);
-	fs.writeFileSync("data.sqlite", buffer);
-
-	res.send('OK');
-	return;
-});
-
-//DELETE USER PROFILE
-app.delete('/users/*', function (req, res)
-{
-	var myName = req.params[0];
-	db.run("DELETE from data WHERE name=:uname", {':uname':myName});
-	//EXPORT
-	var data = db.export();
-	var buffer = new Buffer(data);
-	fs.writeFileSync("data.sqlite", buffer);
-	res.send('OK');
-});
-
-var server = http.listen(3000, function()
-{	
-	var port = server.address().port;
-	console.log('Server started at http://localhost:%s/', port);
-});
